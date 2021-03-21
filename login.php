e1e7bd8a7c9ce3516e3df9d4b1233d00f35d5872
legendsPFS <79815297+legendsPFS@users.noreply.github.com>
2021-03-21 18:31:30 -0400

    font and button size testing & pin code so far
    pin code not working yet, but testing in process
    testing font size and line spacing on phone for button and text

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
-    if ($_POST["pincode"] === "8531") {
-        http_response_code(200);
-    } else{
-        http_response_code(401);
-    }
+
+if ($_POST["pincode"] === "8531") {
+    http_response_code(200);
+} else{
+    http_resposne_code(401);
+}
